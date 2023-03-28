b530d118169d72fa883559806d03e371a6c75117
mckey <miguel.angel.alvarez.mdo@gmail.com>
2023-03-28 03:48:35 -0600

    Se realiza parte03 Ejercicio03 1 al n entre []

--- JS-04 - Arrays and Loops/Ejer03/js/main.js
+++ JS-04 - Arrays and Loops/Ejer03/js/main.js
@@ -11,5 +11,13 @@ console.log("++++++ Imprimir números impares entre 1 - 100 ++++++");
 let impar = 1;          //Inicio
 while (impar<100) {     //Condición
     console.log(impar);
-    impar+= 2;          //Decremento
+    impar+= 2;          //Incremento
 }//Ciclo While de impar
+
+console.log("++++++ Imprimir del 1 al n entre [] ++++++");
+let n=6;
+let corch= 1;          //Inicio
+while (corch<=n) {     //Condición
+    console.log("[" + corch + "]");
+    corch++;          //Incremento
+}//Ciclo While de corch
