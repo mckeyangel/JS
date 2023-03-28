d63854c8b5bec00512e147c72678d0dfffd617d3
mckey <miguel.angel.alvarez.mdo@gmail.com>
2023-03-28 03:21:47 -0600

    Se realiza parte01 Ejercicio03

---
+++ JS-04 - Arrays and Loops/Ejer03/js/main.js
@@ -0,0 +1,7 @@
+console.log("+Imprimir repetidamente decremento xValue en 0.5+");
+
+let xValue=10;          //Inicio
+while (xValue>0) {     //Condición
+    console.log(xValue);
+    xValue-= .5;          //Incemento/Decremento
+}//Ciclo While de cont
