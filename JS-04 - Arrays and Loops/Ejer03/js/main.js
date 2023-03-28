826a5c017fa6a76b25806a7fb9ecb101157f4513
mckey <miguel.angel.alvarez.mdo@gmail.com>
2023-03-28 03:30:52 -0600

    Se realiza parte02 Ejercicio03 impares en consola

--- JS-04 - Arrays and Loops/Ejer03/js/main.js
+++ JS-04 - Arrays and Loops/Ejer03/js/main.js
@@ -1,7 +1,15 @@
-console.log("+Imprimir repetidamente decremento xValue en 0.5+");
+console.log("+++++ Imprimir repetidamente decremento xValue en 0.5 +++++");
 
-let xValue=10;          //Inicio
+let xValue = 10;          //Inicio
 while (xValue>0) {     //Condición
     console.log(xValue);
-    xValue-= .5;          //Incemento/Decremento
-}//Ciclo While de cont
+    xValue-= .5;          //Decremento
+}//Ciclo While de xValue
+
+console.log("++++++ Imprimir números impares entre 1 - 100 ++++++");
+
+let impar = 1;          //Inicio
+while (impar<100) {     //Condición
+    console.log(impar);
+    impar+= 2;          //Decremento
+}//Ciclo While de impar
