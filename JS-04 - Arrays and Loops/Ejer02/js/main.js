5fc778f9089b0365cdb5b457dffa03822214083a
mckey <miguel.angel.alvarez.mdo@gmail.com>
2023-03-28 03:03:27 -0600

    Se realiza Ejercicio02 de Pirámide en la consola

---
+++ JS-04 - Arrays and Loops/Ejer02/js/main.js
@@ -0,0 +1,10 @@
+
+console.log("+++++++++Pirámide+++++++++");
+
+for (let i = 0 ; i < 5 ; i++) {
+    let a="*";
+    for (let j = 0 ; j < i ; j++) {
+        a+="*";    
+    }//for j
+    console.log(a);
+}//for i
